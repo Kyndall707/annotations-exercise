a13785bad672a3eae690a7e748e9082ac0cfb754
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-01 10:02:12 -0500

    fixed warnings with Suppressed Warnings

--- src/Author.java
+++ src/Author.java
@@ -1,4 +1,4 @@
-/**
+/*
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 import java.util.ArrayList;
@@ -16,14 +16,17 @@ public class Author extends Person {
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
+    @SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
 
+    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
